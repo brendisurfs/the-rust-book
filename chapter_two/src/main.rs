8d2a674c6e314890c48d59ab5d1d37872de54a59
Brendan Prednis <brendan.prednis@gmail.com>
2021-07-01 00:24:36 -0700

    (chapter_two): added initial guessing game funcitonality.

--- chapter_two/src/main.rs
+++ chapter_two/src/main.rs
@@ -1,3 +1,19 @@
+// programming a guessing game
+
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess: ");
+
+    // initialize a new String, make it mutable.
+    let mut guess = String::new();
+
+    // take the user input
+    // - &mut guess just means that we can reference guess multiple places. 
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read input.");
+
+    println!("Your guess: {}", guess);    
 }
